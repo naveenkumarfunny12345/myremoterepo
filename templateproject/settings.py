1e29749855d9898492ef8f8dd3a4fd5f044aef36
naveen <naveensree30@gmail.com>
2022-02-22 21:00:45 +0530

    new

--- templateproject/settings.py
+++ templateproject/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATE_DIR = BASE_DIR /'templates'
-STATIC_DIR=  BASE_DIR /'static'
+STATIC_DIR=  BASE_DIR /'staticss'
 
 
 # Quick-start development settings - unsuitable for production
